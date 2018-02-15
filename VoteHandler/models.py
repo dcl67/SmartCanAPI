ca942752c5ea1e377b5069a81dfa3702e0baf935
Karsonic <karsonic7283@gmail.com>
2018-02-15 02:43:59 -0500

    Added placeholder pages and some barebones logic

--- VoteHandler/models.py
+++ VoteHandler/models.py
@@ -6,7 +6,7 @@ import uuid
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     class Meta:
         verbose_name_plural = "categories"
@@ -14,11 +14,41 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Disposable(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
+
+    def get_top_category(self):
+        """
+        Returns the Category that had the most votes on this Disposable.
+        """
+        #  select_related cache's these attributes when it does the query so we get the
+        #  entry and the foreign fields we want to use later all in one query
+        votes = DisposableVote.objects.filter(disposable=self.id).select_related('category')
+        top_category, max_count = None, 0
+        for vote in votes:
+            if vote.count > max_count:
+                top_category = vote.category
+                max_count = vote.count
+        return top_category
+
+    def get_top_votes(self, slice_size=3):
+        """
+        Returns up to slice_size of the top votes for this Disposable as a list of tuples
+        in the form ('category', count).
+        """
+        votes = DisposableVote.objects.filter(disposable=self.id).select_related('category')
+        d = {v.category.name: v.count for v in votes} 
+        return sorted(d.items(), key=lambda x: x[1])[:slice_size]
+
+    def save(self, *args, **kwargs):
+        """Force name to be lowercase"""
+        self.name = self.name.lower()
+        return super(Disposable, self).save(*args, **kwargs)
+
 
 class DisposableVote(models.Model):
     disposable = models.ForeignKey(Disposable, on_delete=models.CASCADE)
@@ -29,12 +59,14 @@ class DisposableVote(models.Model):
         unique_together = ('category','disposable')
 
     def __str__(self):
-        return "{0} has {1} votes for {2}".format(disposable, count, category)
+        return "\'{0}\' has {1} votes for \'{2}\'".format(
+            self.disposable, self.count, self.category)
         # TODO: When Kevin fixes his venv to use Python 3.6 we can use this again
         # return f"{disposable} has {count} vote{count > 1 ? 's' : ''} for {category}"
 
+
 class Configuration(models.Model):
-    can_id = models.UUIDField(verbose_name='Smartcan ID')
+    can_id = models.UUIDField(verbose_name='Smartcan ID', unique=True)
     config = models.TextField(max_length=4096)
 
     @staticmethod
--- VoteHandler/views.py
+++ VoteHandler/views.py
@@ -1,17 +1,41 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import urllib.parse
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from django.http import HttpResponse, HttpResponseRedirect
+
+from .models import Category, Disposable, DisposableVote
+
+# TODO: Write some tests!
+
+def dispose(request):
+    try:
+        user_text = request.POST.get('disposable_item', '')
+        if not user_text:
+            return render(request, 'VoteHandler/home.html', {'error_message' : "Please enter text."})
+        disposeable = Disposable.objects.get(name=user_text.lower())
+    except Disposable.DoesNotExist:
+        return HttpResponseRedirect(reverse('VoteHandler:categorize', args=(user_text,)))
+    else:
+        top_category_id = disposeable.get_top_category().id
+        votes_tuples = disposeable.get_top_votes()
+        return HttpResponseRedirect("{0}?{1}".format(reverse('VoteHandler:result', args=(disposeable.id, top_category_id)), 
+            urllib.parse.urlencode(votes_tuples)))
+
+def categorize(request, disposable_name):
+    return render(request, 'VoteHandler/categorize.html', 
+        {'disposable_name' : disposable_name})
 
 def home(request):
     return render(request, 'VoteHandler/home.html')
 
-def dispose(request):
-    pass
-
 def result(request, disposable_name, category_name):
-    pass
+    votes = request.GET
+    return render(request, 'VoteHandler/result.html', 
+    {'disposable_name' : Disposable.objects.get(id=disposable_name).name,
+     'category_name' : Category.objects.get(id=category_name).name,
+     'votes' : votes})
 
-    # def from_speech(request):
-    #     pass
+# def from_speech(request):
+#     pass
