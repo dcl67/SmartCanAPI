17eca00335c126b5d1f7edca865db8e21eb7a19e
Karsonic <karsonic7283@gmail.com>
2018-02-14 20:44:20 -0500

    Added a home page with a text input

--- VoteHandler/views.py
+++ VoteHandler/views.py
@@ -4,5 +4,14 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
-    def recycle_votes(request, pk):
-        pass
+def home(request):
+    return render(request, 'VoteHandler/home.html')
+
+def dispose(request):
+    pass
+
+def result(request, disposable_name, category_name):
+    pass
+
+    # def from_speech(request):
+    #     pass
