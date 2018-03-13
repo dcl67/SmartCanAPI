509826c05dc930ddd73eb254ded111278cccf761
dcl67 <danny.lopez1094@gmail.com>
2018-03-12 21:27:36 -0400

    Created base.html See comments for how it works, should help with uniform pages

--- Config/urls.py
+++ Config/urls.py
@@ -4,10 +4,12 @@ from . import views
 
 app_name = "Config"
 urlpatterns = [
-    path('', views.configlanding, name='configLanding'),
+    path('', views.home, name='config_home'),
+    path('configlist/', views.configlist, name='configlist'),
     path('<int:smartcan_id>/configure', views.configure, name='configure'), #configure view not yet defined
     path('<int:smartcan_id>/configure/submit', views.submit_configuration, name='submit_configure'), # submit view for configure not yet defined
     path('configure/bin', views.configure_bins, name='configure_bin'),
+    path('configure/bin/<int:pk>', views.edit_bin_config, name='edit_bin'),
     path('<int:smartcan_id>/statistics/', views.statistics, name='statistics'), #statistics view not yet defined
     path('register/<int:smartcan_id>/', views.register, name='register'), #configure not yet defined
     path('redirect', views.redirect, name='redirect'), #configure not yet defined
--- Config/views.py
+++ Config/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import CanInfo, Bin
 from .forms import ConfigurationForm
 
-def configlanding(request):
-    return HttpResponse('foo')
+def home(request):
+    return render(request, 'landing.html')
+
+def configlist(request):
+    can_id = request.POST.get('can_id')
+    print(can_id)
+    if not can_id:
+        return render(request, 'landing.html',
+            {'error_message' : 'Please enter text.'}
+        )
+    can = CanInfo.objects.filter(can_id=can_id.lower())
+
+
 
 def configure(request, smartcan_id):
     # Skeleton getters for now, we can build these out once we define 
@@ -14,20 +26,32 @@ def configure(request, smartcan_id):
     form=ConfigurationForm(request.POST or None, instance=instance)
     print(form)
 
+
 def configure_bins(request):
-    form=ConfigurationForm(request.POST)
-    if request.method=='POST':
+    form = ConfigurationForm(request.POST)
+    if request.method == 'POST':
         if form.is_valid():
-            sID=form.cleaned_data['sID']
+            sId=form.cleaned_data['sId']
             bin_num=form.cleaned_data['bin_num']
             category=form.cleaned_data['category']
-            Bin.objects.create(sId=sID,bin_num=bin_num,category=category,)
+            Bin.objects.create(sId=sId,bin_num=bin_num,category=category,)
             return HttpResponseRedirect(reverse('config_detail', kwargs={'pk':pk}))
     else:
         form=ConfigurationForm()
     return render(request,'configure.html', {'form':form})
-    #if form.is_valid():
-        
+
+
+def edit_bin_config(request, pk):
+    bin_config = get_object_or_404(Bin, pk=pk)
+    form = ConfigurationForm(request.POST, instance=bin_config)
+    if form.is_valid():
+        sID=form.cleaned_data
+        bin_num=form.cleaned_data['bin_num']
+        category=form.cleaned_data['category']
+        form.save()
+        return HttpResponseRedirect(reverse('config_detail', kwargs={'pk':pk}))
+    return render(request,'configure.html',{'form':form})
+
 def bin_config_detail(request, pk):
     configuration=get_object_or_404(Bin,pk=pk)
     context = {
