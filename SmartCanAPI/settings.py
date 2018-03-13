d5d4e2f1b617816e90687e76119e2eb3bd1fd40f
dcl67 <danny.lopez1094@gmail.com>
2018-03-13 00:02:30 -0400

    Pointed settings to templates base dir

--- SmartCanAPI/settings.py
+++ SmartCanAPI/settings.py
@@ -87,7 +87,8 @@ ROOT_URLCONF = 'SmartCanAPI.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        #'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
