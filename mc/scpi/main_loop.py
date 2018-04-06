99aef2ff4947e892043970ef4489cc4255df8ebe
karsonic <karsonic7283@gmail.com>
2018-04-06 16:56:41 -0400

    Added the script to be run on the pi

---
+++ mc/scpi/main_loop.py
@@ -0,0 +1,46 @@
+import asyncio
+
+from lid_controller import LidController
+from motor_controller import MotorController
+from resistor_reader import ResistorReader
+
+async def add_to_queue(bin_q, bin_num, delay_s=0):
+    """
+    Add a bin number to the queue. Can specify a delay in seconds before adding
+    the bin number to the queue.
+    """
+    await asyncio.sleep(delay_s)
+    await bin_q.put(bin_num)
+
+async def move_consumer(bin_q, lid_controller: LidController):
+    """
+    Run the next move in the queue.
+    Will yield and wait if the Queue is empty.
+    """
+    while True:
+        bin_num = await bin_q.get()
+        print(f"Consuming the move to bin #{bin_num}")
+        await lid_controller.open(bin_num)
+        await asyncio.sleep(10)
+        await lid_controller.close()
+
+def main():
+    # Initialize devices
+    top_rr = ResistorReader(spi_port=0, spi_device=0)
+    top_mc = MotorController(top_rr, fwd_pin=2, rev_pin=3)
+    btm_rr = ResistorReader(spi_port=1, spi_device=1)
+    btm_mc = MotorController(btm_rr, fwd_pin=4, rev_pin=5)
+    lc = LidController(top_mc, btm_mc)
+
+    # TODO: Registration
+
+    # TODO: Setup pedal events with GPIO
+
+    # TODO: add websockets loop and pedal listener
+    bin_q = asyncio.Queue()
+    tasks = [asyncio.ensure_future(move_consumer(bin_q, lc)),]
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.gather(*tasks))
+
+if __name__ == '__main__':
+    main()
