043e7cad39c40e5899c869a914af64dfd53a59ed
karsonic <karsonic7283@gmail.com>
2018-04-19 04:43:52 -0400

    doscstrings and 3.6 sugar

--- django/VoteHandler/models.py
+++ django/VoteHandler/models.py
@@ -1,12 +1,25 @@
-# -*- coding: utf-8 -*-
+"""Models for the /api/ that handle categorization and disposal
+
+    Models:
+        Category -- the various waste categories
+        Disposable -- an item that can be disposed of
+        DisposableVote -- the total votes an object has recieved for a category
+"""
+
 from __future__ import unicode_literals
-import uuid
 
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.db.models import QuerySet
 
 # Create your models here.
 class Category(models.Model):
+    """Model representing the various waste categories
+
+    Attriburtes:
+        name {str} -- The name of the category. Ex. Metal
+    """
+
     name = models.CharField(max_length=100, unique=True)
 
     class Meta:
@@ -17,59 +30,90 @@ class Category(models.Model):
 
 
 class Disposable(models.Model):
+    """Model representing an item that can be disposed of
+
+    Arguments:
+        models {[type]} -- [description]
+    """
+
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
-    def get_top_category(self):
+    def get_top_category(self) -> Category:
+        """Returns the Category that had the most votes on this Disposable
+
+        Returns:
+            Category -- Disposable with the most votes
         """
-        Returns the Category that had the most votes on this Disposable.
-        """
-        #  select_related cache's these attributes when it does the query so we get the
-        #  entry and the foreign fields we want to use later all in one query
-        votes = DisposableVote.objects.filter(disposable=self.id).select_related('category')
-        top_category, max_count = None, 0
+        #  select_related caches these attributes when it does the query so we
+        #  get the entry and the foreign fields we want to use later all in one
+        #  query
+        votes = DisposableVote.objects.filter(disposable=self.id) \
+                                      .select_related('category')
+        top_category = None
+        max_count = 0
         for vote in votes:
             if vote.count > max_count:
                 top_category = vote.category
                 max_count = vote.count
         return top_category
 
-    def get_top_votes(self, slice_size=None):
+    def get_top_votes(self, slice_size: int = None) -> QuerySet:
+        """Returns up to slice_size of the top vote counts for this disposable
+
+        Keyword Arguments:
+            slice_size {int} -- The number of items to take from the front of
+                the list. (default: {None})
+
+        Returns:
+            QuerySet[Disposable] -- The slice of disposables with the most votes
         """
-        Returns up to slice_size of the top vote counts for this Disposable 
-        as a QuerySet of Disposeable.
-        """
+        # TODO: Check that this would return the highest, not just the first
         votes = DisposableVote.objects.filter(disposable=self.id).select_related('category')
-        if slice_size:
+        if slice_size is not None:
             return votes[:slice_size]
-        else:
-            return votes
+
+        return votes
 
     def save(self, *args, **kwargs):
-        """Force name to be lowercase"""
+        """Forces name to be lowercase"""
         self.name = self.name.lower()
         return super(Disposable, self).save(*args, **kwargs)
 
 
 class DisposableVote(models.Model):
+    """Model representing the total votes an object has recieved for a category
+
+    Attributes:
+        disposable {Disposable} -- The item that received votes
+        category {Cateogry} -- The category that was voted for
+        count {int} -- The total number of votes for this combination
+
+    Restrictions:
+        category and disposable are unique_together
+    """
+
     disposable = models.ForeignKey(Disposable, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     count = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
     class Meta:
-        unique_together = ('category','disposable')
+        unique_together = ('category', 'disposable')
 
     def __str__(self):
-        return "\'{0}\' has {1} votes for \'{2}\'".format(
-            self.disposable, self.count, self.category)
-        # TODO: When Kevin fixes his venv to use Python 3.6 we can use this again
-        # return f"{disposable} has {count} vote{count > 1 ? 's' : ''} for {category}"
+        return f"{self.disposable} has {self.count} votes for {self.category}"
 
-    def add_votes(self, count):
-        """Adds the number to this objects count then save. Returns nothing."""
+    def add_votes(self, count) -> None:
+        """Adds the number to this objects count then save.
+
+        Arguments:
+            count {int} -- The number of votes to add to the count
+
+        Returns:
+            None
+        """
         if count < 0:
             count = 0
         self.count += count
         self.save()
-
