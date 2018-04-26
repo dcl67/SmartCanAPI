a321a02deb310e29b3379352f90a6dea6b5f28d5
karsonic <karsonic7283@gmail.com>
2018-04-26 00:48:40 +0000

    Fixed errors in script by running on pi. Button works

--- mc/scpi/gpio_test.py
+++ mc/scpi/gpio_test.py
@@ -4,22 +4,27 @@ try:
 except RuntimeError:
     print("Error importing RPi.GPIO! Probably need to sudo this.")
 
-BTN_CHAN = 13
+BTN_CHAN = 40
 
 def setup():
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(BTN_CHAN, GPIO.IN, pull_up_down=GPIO_UP)
+    GPIO.setup(BTN_CHAN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     # Don't worry about other things going on on the board
     GPIO.setwarnings(False)
 
-def callback():
+def callback(channel):
     print('Btn was pressed!')
 
 def main():
+    setup()
+
     # Setup event callback
-    GPIO.add_event_detected(BTN_CHAN, GPIO.RISING, callback=callback,
-                            bouncetime=200)
+    GPIO.add_event_detect(BTN_CHAN, GPIO.RISING, callback=callback,
+                            bouncetime=50)
+
+    while True:
+        pass
 
     # Be nice and cleanup
     GPIO.cleanup()
