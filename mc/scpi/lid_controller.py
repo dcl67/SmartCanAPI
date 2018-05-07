ef16e07c85040cda69ee499e8e419dbb7f3aa3d4
Kevin Rosenberg <karsonic7283@gmail.com>
2018-05-07 00:05:19 -0400

    Part of #75

--- mc/scpi/lid_controller.py
+++ mc/scpi/lid_controller.py
@@ -1,6 +1,5 @@
-import asyncio
-
 from motor_controller import MotorController
+
 
 class LidController():
     """A class that wraps the top and bottom lid MotorControllers"""
@@ -11,7 +10,7 @@ class LidController():
 
     async def close(self):
         """
-        Move the bottom lid to match the position of the top lid, closing the 
+        Move the bottom lid to match the position of the top lid, closing the
         opening. Returns nothing.
         """
         await self.btm_mc.move_to_bin(self.top_mc.get_curr_bin())
@@ -24,15 +23,18 @@ class LidController():
         left = (bin_num - 1) % num_bins
         right = (bin_num + 1) % num_bins
         right_pos = [x % num_bins for x in range(right, num_bins // 2 + right)]
-        if self.btm_mc._curr_pos == left or self.btm_mc._curr_pos == right:
-            return 
+        if self.btm_mc.get_curr_bin() == left or \
+           self.btm_mc.get_curr_bin() == right:
+            return
         elif bin_num in right_pos:
             await self.btm_mc.move_to_bin(right)
         else:
             await self.btm_mc.move_to_bin(left)
-        
+
 
     def _validate(self):
         if self.top_mc.num_bins != self.btm_mc.num_bins:
-            raise ValueError("Number of bins does not match:" +
-             f"top_mc {self.top_mc.num_bins} != btm_mc {self.btm_mc.num_bins}")
+            raise ValueError(
+                "Number of bins does not match:" +
+                f"top_mc {self.top_mc.num_bins} != btm_mc {self.btm_mc.num_bins}"
+            )
